81ea3db4ea6cb53c38f411ac0ee54641c96fe5bf
Ruyllex <ruymori630@gmail.com>
2024-01-03 16:10:14 -0300

    responsive

--- porfolio.js
+++ porfolio.js
@@ -1,3 +1,7 @@
 function redirect(ref){
     window.location.href = ref;
 }
+document.getElementById('mobile-menu').addEventListener('click', function() {
+    var navList = document.querySelector('.nav-list');
+    navList.classList.toggle('show');
+});
