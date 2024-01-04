9f285359af876493a72347ac778d266d7322e2fb
Ruyllex <ruymori630@gmail.com>
2024-01-04 11:09:49 -0300

    menu responsive

--- porfolio.js
+++ porfolio.js
@@ -1,7 +1,25 @@
+const menu = document.querySelector("#menu");
+const open = document.querySelector("#open");
+const close = document.querySelector("#close");
+
+function toggleMenu(isOpen) {
+    if (isOpen) {
+        menu.classList.add("visible");
+    } else {
+        menu.classList.remove("visible");
+    }
+}
+open.addEventListener("click", () => {
+    console.log("Botón Abrir clickeado");
+    toggleMenu(true);
+});
+
+close.addEventListener("click", () => {
+    console.log("Botón Cerrar clickeado");
+    toggleMenu(false);
+});
+
 function redirect(ref){
     window.location.href = ref;
 }
-document.getElementById('mobile-menu').addEventListener('click', function() {
-    var navList = document.querySelector('.nav-list');
-    navList.classList.toggle('show');
-});
+
