f3689e5d512542a9d15a98a27bdf342029289474
Ruy Mori <ruymori630@gmail.com>
2025-09-11 12:03:18 -0300

    fixing align

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,18 +4,20 @@ function About() {
   return (
     <section id="about">
       <h1 className="title">About me</h1>
-      <h2 id="title2"><BlurText text="Hi, my name is:" /></h2>
-      <h3 id="NAME"><BlurText text="RUY MORI" animateBy="letters" className="name-letters" /></h3>
-      <BlurText
-        id="aboutMe"
-        className="about-paragraph"
-        animateBy="letters"
-        direction="bottom"
-        delay={18}
-        text={
-          'A student of Computer Engineering at the University of Buenos Aires. I find joy in crafting digital experiences that resonate with users. Beyond coding, I find balance through fitness and sports like baseball and soccer. In my free time I like to solve LeetCode challenges or read books.'
-        }
-      />
+      <div className="intro">
+        <h2 id="title2"><BlurText text="Hi, my name is:" /></h2>
+        <h3 id="NAME"><BlurText text="RUY MORI" animateBy="letters" className="name-letters" /></h3>
+        <BlurText
+          id="aboutMe"
+          className="about-paragraph"
+          animateBy="letters"
+          direction="bottom"
+          delay={18}
+          text={
+            'A student of Computer Engineering at the University of Buenos Aires. I find joy in crafting digital experiences that resonate with users. Beyond coding, I find balance through fitness and sports like baseball and soccer. In my free time I like to solve LeetCode challenges or read books.'
+          }
+        />
+      </div>
     </section>
   )
 }
